c4f28435eb1a1bd784964c2212d7fab17246444c
sotachi33 <131327009+sotachi33@users.noreply.github.com>
2023-11-20 15:22:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // JavaScript
 
-const map = L.map('map').setView([33.66719252628797, 130.44390437674102], 55);
+const map = L.map('map').setView([33.67345752177457, 130.44140661378802], 15);
+//拡大率は0〜18で設定
 
 // タイルレイヤーを作成し、地図にセットする
 
 // Open Street Map
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
 // 国土地理院
 // L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
 //   maxZoom: 18,
 //   attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
 // }).addTo(map);
+
+// Open Street Map hot
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+}).addTo(map);
 
 //アイコン
 // const whiteIcon = L.icon({
@@ -40,7 +46,8 @@ const circleIcon = L.Icon.extend({
 });
 
 const whiteIcon = new circleIcon({ iconUrl: 'images/ico.png' }),
-  pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' });
+  pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' }),
+  blueIcon = new circleIcon({ iconUrl: 'images/ico_blue.png' });
 
 L.marker([33.67345752177457, 130.44140661378802], { icon: whiteIcon }).addTo(map)
   .bindPopup('A pretty CSS popup.<br> Easily customizable.<br><img src="images/img01.png" alt="img">');
@@ -50,5 +57,38 @@ L.marker([33.67345752177457, 130.44140661378802], { icon: whiteIcon }).addTo(map
 L.marker([33.65958150849491, 130.4440143454105], { icon: pinkIcon }).addTo(map)
   .bindPopup('A pretty CSS popup.<br> Easily customizable.');
 
-L.marker([33.67020170623659, 130.4345714724636], { icon: pinkIcon }).addTo(map)
+L.marker([33.67020170623659, 130.4345714724636], { icon: blueIcon }).addTo(map)
   .bindPopup('A pretty CSS popup.<br> Easily customizable.');
+
+// 円の表示 
+const circle = L.circle([33.67345752177457, 130.44140661378802], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.3,
+  radius: 1000
+}).addTo(map);
+
+circle.bindPopup("駅から1kmの範囲");
+
+// 多角形の表示
+const polygon = L.polygon([
+  [33.665532, 130.434837],
+  [33.668818, 130.465479],
+  [33.701207, 130.438957]
+], {
+  color: 'blue',
+  fillColor: '#30f',
+  fillOpacity: 0.3
+}).addTo(map);
+
+// クリック位置の緯度経度表示
+const popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("ここは " + e.latlng.toString() + " です")
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
